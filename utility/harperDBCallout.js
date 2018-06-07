c54753663ff15db6984fcb40ce206a580d811a4f
Ethan Arrowood <ethan.arrowood@gmail.com>
2018-06-07 15:13:02 +1000

    improve async flow

--- utility/harperDBCallout.js
+++ utility/harperDBCallout.js
@@ -1,33 +1,38 @@
+'use strict'
+
 const { HarperDBConnect } = require('harperdb-connect');
 
-function callHarperDB(call_object, operation, callback){
+async function callHarperDB(call_object, operation, callback){
     const db = new HarperDBConnect();
     
-    db.setAuthorization(call_object.username, call_object.password)
+    try {
+        db.setAuthorization(call_object.username, call_object.password)
+        
+        const regex = /^(https?:\/\/)/gm;
+        const {endpoint_url, endpoint_port} = call_object;
+        const url = regex.test(endpoint_url) ? (
+            `${endpoint_url}:${endpoint_port}`
+        ) : (
+            `http://${endpoint_url}:${endpoint_port}`
+        );
 
-    const regex = /^(https?:\/\/)/gm;
-    const {endpoint_url, endpoint_port} = call_object;
-    const url = regex.test(endpoint_url) ? (
-        `${endpoint_url}:${endpoint_port}`
-    ) : (
-        `http://${endpoint_url}:${endpoint_port}`
-    )
+        await db.connect(url);
 
-    db.connect(url)
-        .then(() => console.log('Connected!'))
-        .catch(err => callback(`Failed to connect to ${url}`, null))
+        db.setDefaultOptions({
+            "method": "POST",
+            "headers": {
+                "content-type": "application/json",
+                "cache-control": "no-cache"
+            },
+            json: true
+        });
 
-    db.setDefaultOptions({
-        "method": "POST",
-        "headers": {
-            "content-type": "application/json",
-            "cache-control": "no-cache"
-        },
-        json: true
-    });
-    db.request(operation, true)
-        .then(res => callback(null, res))
-        .catch(err => callback(err, null))
+        let res = await db.request(operation, true);
+
+        callback(null, res);
+    } catch (err) {
+        callback(err, null);
+    }
 }
 
 module.exports = {
